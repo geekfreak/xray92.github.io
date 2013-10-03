c04881098044734ed5d2787cbf78bf3b09741529
geekfreak <geekfreak@gmail.com>
2013-10-03 21:29:05 +0200

    initial commit

---
+++ aov/aov_menu_data.js
@@ -0,0 +1,151 @@
+/*-------------------------------------------
+Copyright Notice
+--------------------------------------------*/
+
+   DQM_Notice = "DHTML QuickMenu, Copyright (c) - 2001, OpenCube Inc. - www.opencube.com"
+
+/*-------------------------------------------
+Required menu Settings
+--------------------------------------------*/
+
+   DQM_sub_menu_width = 130
+
+   DQM_urltarget = "_self"
+   
+   DQM_onload_statement = ""
+   DQM_codebase = ""
+
+   DQM_border_color = "#104451"
+   
+   DQM_menu_bgcolor = "#B6CAD2"
+   DQM_hl_bgcolor   = "#B6CAD2"
+
+   DQM_sub_xy = "0,0"
+   DQM_border_width = 2
+   DQM_divider_height = 1
+   
+     /*---IE5-MAC Offset Fix - The following two
+   -----parameters correct a position reporting
+   -----bug in IE5 on the Mac platform. Adjust
+   -----the offsets below untill the first level
+   -----sub menus pop up in the correct location.*/
+   
+   DQM_ie5mac_offset_X = 0
+   DQM_ie5mac_offset_Y = -2
+   
+   DQM_ie4mac_offset_X = -8
+   DQM_ie4mac_offset_Y = -50
+
+   DQM_nn4_reaload_after_resize = true
+
+   DQM_nn4_resize_prompt_user = false
+   DQM_nn4_resize_prompt_message = "To reinitialize the navigation menu please click the 'Reload' button."
+   
+/*-------------------------------------------
+Required font Settings
+--------------------------------------------*/
+
+   DQM_textcolor = "#104451"
+   DQM_fontfamily = "Verdana"
+   DQM_fontsize = 10
+   DQM_textdecoration = "normal"
+   DQM_fontweight = "normal"
+   DQM_fontstyle = "normal"
+   DQM_hl_textcolor = "#ffffff"
+   DQM_hl_textdecoration = "normal"
+
+   DQM_margin_top = 3
+   DQM_margin_bottom = 3
+   DQM_margin_left = 5
+
+/*-------------------------------------------
+Optional main menu rollover images
+--------------------------------------------*/
+   
+   DQM_rollover_image0 = rootFolder+"images/button_artists2.gif";
+   DQM_rollover_wh0 = "58,22";
+   DQM_sub_xy0 = "-56,19";
+   DQM_sub_menu_width0 = 120;
+   DQM_subdesc0_0 = "by location"           ; DQM_url0_0 = rootFolder+"location.html"
+   DQM_subdesc0_1 = "artist index"          ; DQM_url0_1 = rootFolder+"a.html"
+
+   DQM_rollover_image1 = rootFolder+"images/button_ceramics2.gif";
+   DQM_rollover_wh1 = "72,22";
+   DQM_sub_xy1 = "-70,19";
+   DQM_sub_menu_width1 = 110;
+   DQM_subdesc1_0 = "all"     				; DQM_url1_0 = rootFolder+"arttype/ceramics/1ceramics.html";
+   DQM_subdesc1_1 = "mosaic"     			; DQM_url1_1 = rootFolder+"arttype/ceramics/1mosaic.html";
+   DQM_subdesc1_2 = "porcelain"     		; DQM_url1_2 = rootFolder+"arttype/ceramics/1porcelain.html";
+   DQM_subdesc1_3 = "pottery"     			; DQM_url1_3 = rootFolder+"arttype/ceramics/1pottery.html";
+   DQM_subdesc1_4 = "stoneware"     		; DQM_url1_4 = rootFolder+"arttype/ceramics/1stoneware.html";
+   DQM_subdesc1_5 = "raku"     				; DQM_url1_5 = rootFolder+"arttype/ceramics/1raku.html"; 
+
+   DQM_rollover_image2 = rootFolder+"images/button_fibers2.gif";
+   DQM_rollover_wh2 = "100,22"
+   DQM_sub_xy2 = "-93,19"
+   DQM_sub_menu_width2 = 110;
+   DQM_subdesc2_0 = "all"     				; DQM_url2_0 = rootFolder+"arttype/fibers/1fibers.html"; 
+   DQM_subdesc2_1 = "batik"     			; DQM_url2_1 = rootFolder+"arttype/fibers/1batik.html";
+   DQM_subdesc2_2 = "lacemaking"    		; DQM_url2_2 = rootFolder+"arttype/fibers/1lacemaking.html";
+   DQM_subdesc2_3 = "quilting"     			; DQM_url2_3 = rootFolder+"arttype/fibers/1quilting.html";
+   DQM_subdesc2_4 = "rugs"     				; DQM_url2_4 = rootFolder+"arttype/fibers/1rugs.html";
+   DQM_subdesc2_5 = "tapestry"     			; DQM_url2_5 = rootFolder+"arttype/fibers/1tapestry.html";
+
+   DQM_rollover_image3 = rootFolder+"images/button_glass2.gif" ;
+   DQM_rollover_wh3 = "49,22"
+   DQM_sub_xy3 = "-43,19"
+   DQM_sub_menu_width3 = 110;
+   DQM_subdesc3_0 = "all"     				; DQM_url3_0 = rootFolder+"arttype/glass/1glass.html"
+   DQM_subdesc3_1 = "etched glass"     		; DQM_url3_1 = rootFolder+"arttype/glass/1etched.html"
+   DQM_subdesc3_2 = "stained glass"     	; DQM_url3_2 = rootFolder+"arttype/glass/1stained.html"
+
+   DQM_rollover_image4 = rootFolder+"images/button_mixedmedia2.gif" 
+   DQM_rollover_wh4 = "91,22"
+   DQM_sub_xy4 = "-87,19"
+   DQM_sub_menu_width4 = 120;
+   DQM_subdesc4_0 = "all"     				; DQM_url4_0 = rootFolder+"arttype/mixedmedia/1mixedmedia.html"
+   DQM_subdesc4_1 = "photography/paint"     ; DQM_url4_1 = rootFolder+"arttype/mixedmedia/1photopaint.html"
+
+   DQM_rollover_image5 = rootFolder+"images/button_original2.gif" ;
+   DQM_rollover_wh5 = "102,22"
+   DQM_sub_xy5 = "-94,19"
+   DQM_sub_menu_width5 = 110;
+   DQM_subdesc5_0 = "all"     				; DQM_url5_0 = rootFolder+"arttype/original/1original.html"
+   DQM_subdesc5_1 = "digital print"     	; DQM_url5_1 = rootFolder+"arttype/original/1digitalprint.html"
+   DQM_subdesc5_2 = "etching"       		; DQM_url5_2 = rootFolder+"arttype/original/1etching.html"
+   DQM_subdesc5_3 = "linotype"       		; DQM_url5_3 = rootFolder+"arttype/original/1linotype.html"
+   DQM_subdesc5_4 = "screenprint"      		; DQM_url5_4 = rootFolder+"arttype/original/1screenprint.html"
+   DQM_subdesc5_5 = "woodblock"       		; DQM_url5_5 = rootFolder+"arttype/original/1woodblock.html"
+   
+   DQM_rollover_image6 = rootFolder+"images/button_paintings2.gif" ;
+   DQM_rollover_wh6 = "76,22"
+   DQM_sub_xy6 = "-71,19"
+   DQM_sub_menu_width6 = 110;
+   DQM_subdesc6_0 = "all"     				; DQM_url6_0 = rootFolder+"arttype/paintings/1paintings.html"
+   DQM_subdesc6_1 = "acrylic"     			; DQM_url6_1 = rootFolder+"arttype/paintings/1acrylic.html"
+   DQM_subdesc6_2 = "airbrush"     			; DQM_url6_2 = rootFolder+"arttype/paintings/1airbrush.html"
+   DQM_subdesc6_3 = "decorative"     		; DQM_url6_3 = rootFolder+"arttype/paintings/1decorative.html"
+   DQM_subdesc6_4 = "oil"     				; DQM_url6_4 = rootFolder+"arttype/paintings/1oil.html"
+   DQM_subdesc6_5 = "watercolor"    		; DQM_url6_5 = rootFolder+"arttype/paintings/1watercolor.html"
+        
+   DQM_rollover_image7 = rootFolder+"images/button_photographs2.gif" ;
+   DQM_rollover_wh7 = "95,22"
+   DQM_sub_xy7 = "-89,19"
+   DQM_sub_menu_width7 = 110;
+   DQM_subdesc7_0 = "all"     				; DQM_url7_0 = rootFolder+"arttype/photographs/1photographs.html"
+   DQM_subdesc7_1 = "black/white"     		; DQM_url7_1 = rootFolder+"arttype/photographs/1blackwhite.html"
+   DQM_subdesc7_2 = "color"     			; DQM_url7_2 = rootFolder+"arttype/photographs/1color.html"
+   DQM_subdesc7_3 = "digital"     			; DQM_url7_3 = rootFolder+"arttype/photographs/1digital.html"
+
+   DQM_rollover_image8 = rootFolder+"images/button_sculpture2.gif" ;
+   DQM_rollover_wh8 = "76,22"
+   DQM_sub_xy8 = "-110,19"
+   DQM_sub_menu_width8 = 110;
+   DQM_subdesc8_0 = "all"     				; DQM_url8_0 = rootFolder+"arttype/sculpture/1sculpture.html"
+   DQM_subdesc8_1 = "bronze"     			; DQM_url8_1 = rootFolder+"arttype/sculpture/1bronze.html"
+   DQM_subdesc8_2 = "clay"     				; DQM_url8_2 = rootFolder+"arttype/sculpture/1clay.html"
+   DQM_subdesc8_3 = "metal"     			; DQM_url8_3 = rootFolder+"arttype/sculpture/1metal.html"
+   DQM_subdesc8_4 = "papier mache"     		; DQM_url8_4 = rootFolder+"arttype/sculpture/1papiermache.html"
+   DQM_subdesc8_5 = "stone"     			; DQM_url8_5 = rootFolder+"arttype/sculpture/1stone.html"
+   
+   
